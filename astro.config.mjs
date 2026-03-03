0723aa24ddf6289e5fce812de3fcfc1d579e5ff1
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2026-03-03 10:22:23 +0100

    fix static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
+import vue from "@astrojs/vue";
 import node from "@astrojs/node";
 
-import vue from "@astrojs/vue";
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
